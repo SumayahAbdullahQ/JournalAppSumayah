1696c0e5fb2e40c5bda3dd8cc94ba21ce8a0dc5b
SumayahAbdullahQ <sumayahabdullah27@gmail.com>
2024-10-24 11:24:54 +0300

    Initial Commit

---
+++ JournalAppTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JournalAppTest
+//
+//  Created by Sumayah Alqahtani on 21/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JournalAppTest/JournalAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppTestApp.swift
+//  JournalAppTest
+//
+//  Created by Sumayah Alqahtani on 21/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
